321b1e01dfee595e7412c109ec3ae25e92ebee11
ckvvanle <phamvanle10dt2.bkdn@gmail.com>
2018-08-03 11:28:39 +0700

    Chapter 7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def hello
     render html: "hello, mysample_app!"
+  end
+
+  def not_found
+    render file: "#{Rails.root}/public/404.html", status: 404
   end
 end
