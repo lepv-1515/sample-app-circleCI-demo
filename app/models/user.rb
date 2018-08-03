24f06f3ef18883c5e1ac96eac5fe6f8dc6bcb4f9
ckvvanle <phamvanle10dt2.bkdn@gmail.com>
2018-08-03 11:28:39 +0700

    Chapter 11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
-  before_save{email.downcase!}
   validates :name, presence: true, length: {maximum: Settings.user.max_name}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: Settings.user.max_email},
@@ -11,32 +10,54 @@ class User < ApplicationRecord
   validates :password, presence: true, allow_nil: true,
                        length: {minimum: Settings.user.min_pass}
 
-  def self.new_token
-    SecureRandom.urlsafe_base64
-  end
+  before_save :downcase_email
+  before_create :create_activation_digest
+  default_scope ->{order(created_at: :desc)}
 
-  def remember
-    self.remember_token = User.new_token
-    update_attribute :remember_digest, digest(remember_token)
-  end
-
-  def authenticated? remember_token
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end
-
-  def forget
-    update_attribute :remember_digest, nil
-  end
-
-  private
-
-  def digest string
+  def self.digest string
     cost = if ActiveModel::SecurePassword.min_cost
              BCrypt::Engine::MIN_COST
            else
              BCrypt::Engine.cost
            end
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? attribute, token
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
+  end
+
+  def activate
+    update_columns activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
