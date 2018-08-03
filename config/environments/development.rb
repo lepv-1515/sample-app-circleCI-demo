af5f23a84499a7fa1bc43cb06e99d704938c0545
ckvvanle <phamvanle10dt2.bkdn@gmail.com>
2018-08-03 11:28:39 +0700

    Chapter 12

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,6 +55,7 @@ Rails.application.configure do
   config.assets.quiet = true
 
   # config email preview
+
   # config.action_mailer.raise_delivery_errors = true
   # config.action_mailer.delivery_method = :test
   # host = "localhost:3000" # use your local dev host
